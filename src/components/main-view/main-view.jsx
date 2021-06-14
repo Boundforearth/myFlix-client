32dec4be02255c0a109ad2d3c739fb4fd1ba187f
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-06-13 22:03:41 -0400

    Added functions to handle click events

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,31 +7,38 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        {_id: 1, Title: "Inception", Description: "desc 1...", ImagePath: "..."},
-        {_id: 2, Title: "The Shawshank Redemption", Description: "desc2...", ImagePath: "..."},
-        {_id: 3, Title: "Gladiator", Description: "desc3...", ImagePath: "..."}
+        {_id: 1, Title: "Inception", Description: "desc 1...", ImagePath: "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg"},
+        {_id: 2, Title: "The Shawshank Redemption", Description: "desc2...", ImagePath: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg"},
+        {_id: 3, Title: "Gladiator", Description: "desc3...", ImagePath: "https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png"}
       ],
       selectedMovie: null
     };
   }
 
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    })
+  }
+
   render() {
     const movies = this.state.movies;
+    const selectedMovie = this.state.selectedMovie;
     if (selectedMovie) {
-      return <MovieView movie={selectedMovie} />;
+      return <MovieView movie={selectedMovie} onBackClick={(newSelectedMovie) => {this.setSelectedMovie(newSelectedMovie);}}/>;
     }
 
-    else if (movies.length === 0) {
+    if (movies.length === 0) {
       return <div className="main-view">The list is empty!</div>
-    } else { 
-      return (
+    }
+    return (
       <div className="main-view">
-        <button onClcik={() => {alert("nice")}}>Click me!</button>
         {movies.map((movie) => {
-          return <MovieCard key={movie._id} movieData={movie}/>;
+           return (<MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { 
+             this.setSelectedMovie(movie)
+            }}/>);
         })}
       </div>
-      );
-    }
+    );
   }
 }
