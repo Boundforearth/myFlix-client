d246c5e6ff9c9d4ada8a7867fc615f0d1d7f2b6d
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-06-20 22:31:32 -0400

    Added prop-types and code change to registration view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     //Send a request to the server for authentication
     //then call props.onLoggedIn(username)
     props.onLoggedIn(username);
+  };
+
+  const needToRegister = (e) => {
+    e.preventDefault();
+    props.notRegistered();
   }
 
-  render() {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <span>
         <button type="button" onClick={handleSubmit}>Submit</button>
-      </form>
-    )
-  }
+        <button type="button" onClick={needToRegister}>Register</button>
+      </span>
+    </form>
+  );
+};
+
+LoginView.propTypes = {
+  notRegistered: PropTypes.func.isRequired,
+  onLoggedIn: PropTypes.func.isRequired
 }
