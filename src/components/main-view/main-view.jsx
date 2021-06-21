43d2c55a4ff8aed5f21501aa1b876bc383863dce
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-06-20 22:30:33 -0400

    Added code to implement the registration view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import axios from "axios";
 
-import { LoginView } from "../login-view/login-view"
-import { MovieCard } from "../movie-card/movie-card"
-import { MovieView } from "../movie-view/movie-view"
+import { RegistrationView } from "../registration-view/registration-view";
+import { LoginView } from "../login-view/login-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
 export class MainView extends React.Component {
 
   constructor() {
@@ -11,16 +13,10 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
-      user: null
+      user: null,
+      registered: true
     };
   }
-
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie
-    })
-  }
-
 
   componentDidMount() {
     axios.get("https://myflix-57495.herokuapp.com/movies")
@@ -34,17 +30,43 @@ export class MainView extends React.Component {
       });
   }
 
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie
+    })
+  }
+
   onLoggedIn(user) {
     this.setState({
       user
     });
   }
 
+  toggleRegisterView() {
+    if(this.state.registered === true) {
+      this.setState({
+        registered: false
+      });
+    }
+    else this.setState({
+      registered: true
+    });
+  }
+
   render() {
     const movies = this.state.movies;
     const selectedMovie = this.state.selectedMovie;
+    const user = this.state.user
+    const registered = this.state.registered;
 
-    if(!user) {return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />};
+   if(!user && (registered === true)) {return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} 
+    notRegistered={() => {
+      this.toggleRegisterView();
+      }} />};
+
+    if(registered === false) {
+      return <RegistrationView onRegistration = {() => this.toggleRegisterView()} />;
+    }
 
     if (movies.length === 0) {
       return <div className="main-view">apples</div>
