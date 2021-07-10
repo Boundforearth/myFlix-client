a220ea12754a9c94fc13d56cfaea0985b4d46c0e
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-07-10 17:12:44 -0400

    Added routing

--- src/components/nav-view/nav-view.jsx
+++ src/components/nav-view/nav-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 //images must be imported with the url:  part before them to work with parcel.
 //These images will be displayed to give users a visual hint about what the view will change to.
@@ -17,8 +18,9 @@ export function NavView(props) {
 
 
   //These variables will be used to set a className
-  let verticalButton
-  let horizontalButton
+  let verticalButton;
+  let horizontalButton;
+  let navigationDisplay;
 
 
   //Both variables must be defined to prevent an empty class.  none is basically just a placeholder
@@ -32,28 +34,32 @@ export function NavView(props) {
     horizontalButton = "none";
   }
 
+  if(!user) {
+    navigationDisplay = "no-nav-bar-block";
+  }
+  else {
+    navigationDisplay = "nav-bar-block"
+  }
+
   const handleChange = (e) => {
     let view = e.target.dataset.value;
     changeView(view);
   }
   return (
-    <Navbar bg="light" fixed="top" expand="md">
-      <Navbar.Brand>myFlix</Navbar.Brand>
+    <Navbar bg="light" fixed="top" expand="md" id={navigationDisplay}>
+      <Navbar.Brand><Link className="link-styling" to="/">myFlix</Link></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title={user} id="basic-nav-dropdown">
-            <NavDropdown.Item as="button" >MyProfile</NavDropdown.Item>
-            <NavDropdown.Item as="button">Favorites</NavDropdown.Item>
+            <NavDropdown.Item as="button"><Link className="link-styling" to={`/users/${user}`}>MyProfile</Link></NavDropdown.Item>
+            <NavDropdown.Item as="button"><Link className="link-styling" to={`/myfavorites/${user}`}>Favorites</Link></NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item as="button" onClick={() => {logout()}}>Log-out</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
-          <Button type="submit" variant="secondary">Submit</Button>
-        </Form>
-        <div id="view-select-block">
+        <div>
+          <span>Select View:  </span>
           <button className={`view-button ${verticalButton}`} ><img alt="View selection image" className="view-choice" alternate="Change to vertical view" onClick={handleChange} data-value="1" src={Vertical}/></button>
           <button className={`view-button ${horizontalButton}`}><img alt="View selection image" className="view-choice" alternate="Change to horizontal view" onClick={handleChange} data-value="2" src={Horizontal}/></button>
         </div>
