9a285fbc3f9538d400b28046e4a1abf77e18bfde
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-07-10 17:14:05 -0400

    Added requests and logic for add/delete button

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./movie-view.scss"
-
+import axios from "axios";
+let addOrDeleteButton
 export class MovieView extends React.Component {
-  
-  keypressCallback(event) {
-    console.log(event.key);
-  }
 
-  componentDidMount() {
-    document.addEventListener("keypress", this.keypressCallback);
+  updateFavorite (value, movie) {
+    let user = localStorage.getItem("user");
+    let token = localStorage.getItem("token");
+    if (value === true) {
+      axios.delete(`https://myflix-57495.herokuapp.com/users/${user}/mylist/${movie}`,
+      {
+        headers: {Authorization: `Bearer ${token}`}})
+      .then(() => {
+        this.props.deleteFavorite(movie);
+        alert("The movie has been deleted from your favorites");
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+    }
+    else {axios.post(`https://myflix-57495.herokuapp.com/users/${user}/mylist/${movie}`, "", 
+    {headers: {Authorization: `Bearer ${token}`}})
+    .then((data) => {
+      this.props.addFavorite(movie);
+      alert("This movie has been added to your favorites!");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
   }
+}
+  checkFavorites(movie, favorites) {
+    let favoritedMovie = favorites.find(favorite => favorite === movie);
+    addOrDeleteButton = null;
+    if(favoritedMovie) {
+      let value = true;
+      addOrDeleteButton = <Button variant="secondary" onClick={() => this.updateFavorite(value, movie)}>Unfavorite Movie</Button>
+    }
+    else {
+      let value = false
+      addOrDeleteButton = <Button variant="secondary" onClick={() => this.updateFavorite(value, movie)}>Favorite Movie</Button>
+    }
+  }
+     
 
-  componentWillUnmount() {
-    document.removeEventListener("keypress", this.keypressCallback)
-  }
 
   render() {
-    const movie = this.props.movie;
-    const onBackClick = this.props.onBackClick;
+    const { movie, onBackClick, favorites} = this.props
+    this.checkFavorites(movie._id, favorites);
+    
     return(
       <Card bsPrefix="movie-view-width">
         <Card.Img 
@@ -30,11 +62,14 @@ export class MovieView extends React.Component {
           />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Genre: {movie.Genre.Name}</Card.Subtitle>
-          <Card.Text>Director: {movie.Director.Name}</Card.Text>
+          <Card.Subtitle className="mb-2 text-muted"><Link to={`/genres/${movie.Genre.Name}`}>Genre: {movie.Genre.Name}</Link></Card.Subtitle>
+          <Card.Text><Link to={`/directors/${movie.Director.Name}`}>Director: {movie.Director.Name}</Link></Card.Text>
           <Card.Text>{movie.Description}</Card.Text>
         </Card.Body>
-        <Button id="movie-view-button" variant="secondary" onClick={() => onBackClick()}>Back</Button>
+        <div className="button-group">
+          <Button id="movie-view-button" variant="secondary" onClick={() => onBackClick()}>Back</Button>
+          {addOrDeleteButton}
+        </div>
       </Card>
     );
   }
@@ -58,5 +93,4 @@ MovieView.propTypes = {
     }).isRequired
 
   }).isRequired,
-  onBackClick: PropTypes.func.isRequired
-};
+}
