9d4b8e33b4e3eb5a42ce75bff25eaf1db6a21750
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-07-10 17:17:23 -0400

    Fixed props

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 import "./director-view.scss";
 
 export class DirectorView extends React.Component {
 render() {
-  const movie = this.props.movie;
+  const director = this.props.director;
+  const movies = this.props.movies
   const onBackClick = this.props.onBackClick;
   return(
     <Card bsPrefix="movie-view-width">
       <Card.Body>
-        <Card.Title>{movie.Director.Name}</Card.Title>
-        <Card.Text>Bio: {movie.Director.Bio}</Card.Text>
-        <Card.Text>{movie.Director.Birth}</Card.Text>
-        <Card.Text>{movie.Director.Death}</Card.Text>
+        <Card.Title>{director.Name}</Card.Title>
+        <Card.Text>Bio: {director.Bio}</Card.Text>
+        <Card.Text>{director.Birth}</Card.Text>
+        <Card.Text>{director.Death}</Card.Text>
       </Card.Body>
       <Button id="movie-view-button" variant="secondary" onClick={() => onBackClick()}>Back</Button>
     </Card>
