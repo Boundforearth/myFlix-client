838411b797cb51e4f93ebddae16d003d5778a6a8
James Rutkowski <j.r.rutkowski52@gmail.com>
2021-07-18 23:40:59 -0400

    Removed all state methods, replaced with redux functionality

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+
+import { setMovies, setUser, setFavorites, setView } from '../../actions/actions';
+
+/* 
+  #1 The rest of components import statements but without the MovieCard's 
+  because it will be imported and used in the MoviesList component rather
+  than in here. 
+*/
+
 
 //import other views to be used
 import { RegistrationView } from "../registration-view/registration-view";
 import { LoginView } from "../login-view/login-view";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
+import MovieView from "../movie-view/movie-view";
+import MoviesList from '../movies-list/movies-list';
 import { GenreView } from "../genre-view/genre-view";
 import { DirectorView } from "../director-view/director-view";
 import { ProfileView } from "../profile-view/profile-view";
@@ -16,43 +26,18 @@ import { NavView } from "../nav-view/nav-view";
 
 import "./main-view.scss";
 
-export class MainView extends React.Component {
+class MainView extends React.Component {
 
   //set the states
   constructor() {
     super();
-    this.state = {
-      movies: [],
-      user: null,
-      currentUserFavorites: [],
-      //Default view will be the vertical cards with images at the top
-      // 1 = vertical cards
-      // 2 = horizontal cards
-      selectedView: 1,
-    };
-  }
-
-  //used to add a new movie to the favorites list
-  addFavorite(movie) {
-    this.setState({
-      currentUserFavorites: [...this.state.currentUserFavorites, movie]
-    })
-  }
-
-  //filters out the selected movie and removes it from the state
-  deleteFavorite(movie) {
-    this.setState({
-      currentUserFavorites: this.state.currentUserFavorites.filter((m) => {return m !== movie})
-    })
   }
 
   //get the movie data when the component mounts
   componentDidMount() {
     let accessToken = localStorage.getItem("token");
     if (accessToken !== null) {
-      this.setState({
-        user: localStorage.getItem("user")
-      });
+        this.props.setUser(localStorage.getItem("user"))
       this.getMovies(accessToken);
       this.getUserFavorites(accessToken)
     }
@@ -65,9 +50,7 @@ export class MainView extends React.Component {
     })
     .then((response) => {
       //Assign the result to the state
-      this.setState({
-        movies: response.data
-      });
+      this.props.setMovies(response.data);
     })
     .catch(function(error) {
       console.log(error);
@@ -79,49 +62,17 @@ export class MainView extends React.Component {
     axios.get(`https://myflix-57495.herokuapp.com/users/${user}`, {
       headers: {Authorization: `Bearer ${token}`}})
     .then((data) => {
-      this.setState({
-        currentUserFavorites: data.data.Favorites
-      })
+        this.props.setFavorites(data.data.Favorites)
     })
     .catch((e) => {
       console.log(e);
     })
   }
 
-  //change the cards between horizontal and vertical
-  setSelectedView(view) {
-    this.setState({
-      selectedView: view
-    })
-  }
-
-  //Use to set the movies state
-  setMovies(array) {
-    this.setState({
-      movies: array
-    })
-  }
-
-  //change the state of the selected movie
-  setSelectedMovie(movie) {
-    this.setState({
-      selectedMovie: movie
-    })
-  }
-
-  // use to set the user state
-  setUser(user) {
-    this.setState({
-      user: user
-    })
-  }
-
   //set the state of the user to who is actually logged in and grab their list of favorite movies
   onLoggedIn(authData) {
-    this.setState({
-      user: authData.user.Username,
-      currentUserFavorites: authData.user.Favorites
-    });
+    this.props.setUser(authData.user.Username);
+    this.props.setFavorites(authData.user.Favorites);
     localStorage.setItem("token", authData.token);
     localStorage.setItem("user", authData.user.Username);
     this.getMovies(authData.token);
@@ -134,17 +85,15 @@ export class MainView extends React.Component {
     //move to login page
     window.open('/', '_self');
     //reset the states
-    this.setState({
-      user: null,
-      selectedView: 1,
-      currentUserFavorites: []
-    });
+    this.props.setUser('');
+    this.props.currentFavorites([])
+    this.props.setView('1');
 
   }
 
   render() {
     //define all the states
-    const { movies, user, selectedView, currentUserFavorites } = this.state
+    let { movies, currentFavorites, user, selectedView} = this.props;
 
     //declare a variable that will be used to set an id for the <Row> component.  This will set the css to change the view
     let selectedViewFlex;
@@ -161,22 +110,15 @@ export class MainView extends React.Component {
     return (
       <Router>
         <NavView selectedView={selectedView} changeView={(view) => {this.setSelectedView(view)}} username={user} onLogout={() => {this.onLoggedOut()}}/>
-        <Row className="main-view justify-content-md-center" id={selectedViewFlex}>
+        <Row className="main-view justify-content-md-center">
           <Route exact path="/" render={() => {
             if(!user) return <Col>
               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
             </Col>
             //assuming there are movies, map each movie to its own MovieCard
             if (!movies.length) return <div className="main-view" />;
-            return movies.map(m => (
-              <Col lg={3} md={4} sm={6} bsPrefix="all-col-sizing" key={m._id}>
-                <MovieCard selectedView={selectedView} movieData={m} />
-              </Col>
-            ))
+            return  <MoviesList movies={movies}/>;
             }} />
-        </Row>
-
-        <Row className="main-view justify-content-md-center">
           <Route path="/register" render={() => {
             //if someone is logged in, send them to the home page instead of the register page
             if (user) return <Redirect to="/" />
@@ -191,7 +133,7 @@ export class MainView extends React.Component {
           </Col>
             if (!movies.length) return <div className="main-view" />;
             return <Col md={6} xs={8}>
-              <MovieView addFavorite={(movie) => this.addFavorite(movie)} deleteFavorite={(movie) => this.deleteFavorite(movie)} favorites={currentUserFavorites} movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
             </Col>
           }}/>
 
@@ -214,7 +156,6 @@ export class MainView extends React.Component {
             if (!movies.length) return <div className="main-view" />;
             return <Col md={6} xs={8}>
               <DirectorView director={movies.find(m => m.Director.Name === match.params.director).Director}
-                movies={movies.filter(m => m.Director.Name === match.params.director)}
                 onBackClick={() => history.goBack()} />
             </Col>
           }}/>
@@ -240,14 +181,14 @@ export class MainView extends React.Component {
             if (!movies.length) return <div className="main-view" />;
 
             //if the user has no favorite movies, display this
-            if (!currentUserFavorites.length) return <h3>Your Favorites List Is Empty</h3>
+            if (!currentFavorites.length) return <h3>Your Favorites List Is Empty</h3>
 
             //Create a list of all the movies with all the information based on the users favorites
             //currentUserFavorites is only ID's, we need all the movie info
             let favoriteMovies = [];
               for(let i = 0; i < movies.length; i++) {
-                for(let j = 0; j < currentUserFavorites.length; j++) {
-                  if(movies[i]._id === currentUserFavorites[j]) {
+                for(let j = 0; j < currentFavorites.length; j++) {
+                  if(movies[i]._id === currentFavorites[j]) {
                     favoriteMovies.push(movies[i]);
                     break;
                   }
@@ -256,7 +197,7 @@ export class MainView extends React.Component {
               //map the new list of movies to their own cards called FavoritesView
             return favoriteMovies.map(m => (
               <Col lg={3} md={4} sm={6} bsPrefix="all-col-sizing" key={m._id}>
-                <FavoritesView setMovies={(array) => this.setMovies(array)} user={user} selectedView={selectedView} movieData={m} favorites={currentUserFavorites}/>
+                <FavoritesView setMovies={(array) => this.setMovies(array)} user={user} selectedView={selectedView} movieData={m} favorites={currentFavorites}/>
               </Col>
             ))
             }} />
@@ -265,3 +206,14 @@ export class MainView extends React.Component {
     );
   }
 }
+
+let mapStateToProps = state => {
+  return { 
+    movies: state.movies,
+    user: state.user,
+    currentFavorites: state.currentFavorites,
+    selectedView: state.selectedView
+   }
+}
+
+export default connect(mapStateToProps, { setMovies, setUser, setFavorites, setView } )(MainView);
